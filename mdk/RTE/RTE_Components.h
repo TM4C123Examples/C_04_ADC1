5ac9382d0b047399a4bc442365a0be4327682d3c
Manuel Limones <limonesu.me@gmail.com>
2015-04-24 13:36:56 -0500

    first commit

---
+++ mdk/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'C_04_ADC1_TIVA_C' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
